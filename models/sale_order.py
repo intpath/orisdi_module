5f08f786409b544f8e8cad9a260aa77cc5ba9be6
M.Saeb <48291783+M-Saeb@users.noreply.github.com>
2021-03-21 17:34:42 +0300

    Removed Shipping_cost_a field

--- models/sale_order.py
+++ models/sale_order.py
@@ -8,16 +8,4 @@ class SaleOrder(models.Model):
 
     governorate = fields.Many2one("res.country.state")
     shipping_company_delivery_number = fields.Char("Shpping Company Delivery Number")
-    shipping_company_delivery_date = fields.Date("Shpping Company Delivery Date")
-    shipping_cost_a = fields.Integer(compute="_get_shipping_price")
-
-    @api.depends('order_line')
-    def _get_shipping_price(self):
-        for order in self:
-            delivery_lines = order.order_line.filtered(lambda x: x.is_delivery)
-            if delivery_lines:
-                order.shipping_cost_a = sum( delivery_lines.mapped('price_subtotal') )
-            else:
-                order.shipping_cost_a = 0
-
-        
+    shipping_company_delivery_date = fields.Date("Shpping Company Delivery Date")        
