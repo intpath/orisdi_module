d24b41fdf2041f58f300d8e83d71da1393989ffe
2xhx6 <hadermusc@gmail.com>
2021-06-01 12:37:10 +0000

    Orisdi Customization 3

--- models/sale_order.py
+++ models/sale_order.py
@@ -10,8 +10,17 @@ class SaleOrder(models.Model):
     shipping_company_delivery_number = fields.Char("Shpping Company Delivery Number")
     shipping_company_delivery_date = fields.Date("Shpping Company Delivery Date")        
 
+    sourcing_date = fields.Datetime('Sourcing Date', readonly=False, copy=False)
+    sourcing_delay = fields.Integer('Sourcing Delay', readonly=False, copy=False)
+
     def action_confirm(self):
         date_order = self.date_order
         res = super(SaleOrder, self).action_confirm()
         self.date_order = date_order
+        self.sourcing_delay = (self.date_order - self.sourcing_date).days
+        return res
+
+    def draft_to_confirm(self):
+        res = super(SaleOrder, self).draft_to_confirm()
+        self.sourcing_date = self.date_order
         return res
